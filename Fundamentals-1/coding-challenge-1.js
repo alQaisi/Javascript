1e97a87e4788d595fb68637a11c0bebfafbe893a
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2023-01-30 17:53:03 +0300

    coding challenge 1

---
+++ Fundamentals-1/coding-challenge-1.js
@@ -0,0 +1,7 @@
+const markWeight=78,johnWeight=92;
+const markTall=1.96,johnTall=1.76;
+
+const markBmi=markWeight/markTall**2;
+const johnBmi=johnWeight/johnTall**2;
+
+const markHigherBMI=markBmi>johnBmi;
