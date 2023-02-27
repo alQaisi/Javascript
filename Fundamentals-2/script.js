65d61e60a8aea3d082c14f1533f041a7654fdb8b
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2023-02-27 16:03:46 +0300

    strict mode breif

---
+++ Fundamentals-2/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+// strict mode will fire an error if we try to assign or use an undefined variable
+// strict mode intorduces a short list of variable names that are reserved for features that might be added to the language later
+
+let hasDriversLicense=false;
+const passTest=true;
+
+// if(passTest)
+//     hasDriversLicense=true; 
+
+if(passTest)
+    hasDriversLicense=true; 
+
+if(hasDriversLicense)
+    console.log('I can drive :D')
+
+// const interface="audio";
+// const private=545
+
