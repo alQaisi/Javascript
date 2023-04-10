25536e990526ce3440729588138cc263fa1adecf
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2023-04-10 17:19:22 +0300

    Fixing reduce method

--- Prototype.js
+++ Prototype.js
@@ -1,10 +1,19 @@
-Array.prototype.reduce2=function(func,initial=0){
-    let acc=initial;
-    for(let i=0;i<this.length;i++){
-        acc=func(acc,this[i],i);
+Array.prototype.reduce2=function(func,initialValue){
+    let array=this;
+    let counter,acc;
+    if(initialValue==undefined){
+        acc=array[0];
+        counter=1;
+    }else{
+        acc=initialValue;
+        counter=0;
+    }
+    for(let i=counter;i<array.length;i++){
+        acc=func(acc,array[i]);
     }
     return acc;
 }
+
 Array.prototype.map2=function(func){
     const newArray=[];
     for(let i=0;i<this.length;i++){
